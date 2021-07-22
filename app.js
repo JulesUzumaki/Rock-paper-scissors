79d74fad154a76159cddfbb465c732bc13cd7541
julie <cabanat.julie@eleves.lfiduras.com>
2021-07-22 09:17:08 +0700

    computer choice done

---
+++ app.js
@@ -0,0 +1,39 @@
+const game = () => {
+    let pScore = 0;
+    let cScore = 0;
+
+    //start the game
+    const startGame = () => {
+        const playBtn = document.querySelector(".intro button");
+        const introScreen = document.querySelector('.intro');
+        const match = document.querySelector('.match');
+
+        playBtn.addEventListener('click', () =>{
+            introScreen.classList.add('fadeOut');
+            match.classList.add('fadeIn');
+        })
+    }
+    //Play match 
+    const playMatch = () => {
+        const options = document.querySelectorAll('.options button');
+        const playerHand = document.querySelector('.player-hand');
+        const computerHand = document.querySelector('.computer-hand');
+        //computer options
+        const computerOptions = ['rock', 'paper', 'scissors'];
+
+        options.forEach((option)=>{
+            option.addEventListener('click', function() {
+                //computer choice
+                const computerNumber = Math.floor(Math.random() * 3);
+                const computerChoice = computerOptions[computerNumber];
+            });
+        });
+    };
+
+    //inner functions
+    startGame();
+    playMatch();
+};
+
+//start the game
+game();
